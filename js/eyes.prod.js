223d3974c17a8e46469876c7b0b101d63e97beba
JkDev <69984125+JkDevArg@users.noreply.github.com>
2022-01-13 21:39:46 -0500

    Obfuscatedddddd

---
+++ js/eyes.prod.js
@@ -0,0 +1,65 @@
+try{
+    const _0x2ba6 = [
+        "\x2E\x62\x61\x6C\x6C",
+        "\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72\x41\x6C\x6C",
+        "\x6D\x6F\x75\x73\x65\x6D\x6F\x76\x65",
+        "\x63\x6C\x69\x65\x6E\x74\x58",
+        "\x69\x6E\x6E\x65\x72\x57\x69\x64\x74\x68",
+        "\x25",
+        "\x63\x6C\x69\x65\x6E\x74\x59",
+        "\x69\x6E\x6E\x65\x72\x48\x65\x69\x67\x68\x74",
+        "\x6C\x65\x66\x74",
+        "\x73\x74\x79\x6C\x65",
+        "\x74\x6F\x70",
+        "\x74\x72\x61\x6E\x73\x66\x6F\x72\x6D",
+        "\x74\x72\x61\x73\x6C\x61\x74\x65\x28\x2D",
+        "\x2C\x2D",
+        "\x29",
+        "\x66\x6F\x72\x45\x61\x63\x68",
+        "\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72",
+        "\x63\x6C\x61\x73\x73\x4E\x61\x6D\x65",
+        "\x6F\x31",
+        "\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64",
+        "\x6F\x6A\x6F",
+        "\x6F\x32",
+        "\x6D\x6F\x75\x73\x65\x6F\x75\x74",
+        "\x6F\x6A\x6F\x2D\x32",
+        "\x6C\x6F\x67",
+    ];
+    eval(
+        (function (p, a, c, k, e, d) {
+            e = function (c) {
+                return (c < a ? "" : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36));
+            };
+            if (!"".replace(/^/, String)) {
+                while (c--) {
+                    d[e(c)] = k[c] || e(c);
+                }
+                k = [
+                    function (e) {
+                        return d[e];
+                    },
+                ];
+                e = function () {
+                    return "\\w+";
+                };
+                c = 1;
+            }
+            while (c--) {
+                if (k[c]) {
+                    p = p.replace(new RegExp("\\b" + e(c) + "\\b", "g"), k[c]);
+                }
+            }
+            return p;
+        })(
+            "y{h n=b[a[1]](a[0]);b[a[k]](a[2],(i)=>{h f=(i[a[3]]*l)/m[a[4]]+a[5];h g=(i[a[6]]*l)/m[a[7]]+a[5];n[a[x]]((e)=>{e[a[9]][a[8]]=f;e[a[9]][a[t]]=g;e[a[9]][a[A]]=a[B]+f+a[D]+g+a[E]})});j w(){b[a[k]](a[2],j(){b[a[d]](a[p])[a[c]]=a[q];b[a[d]](a[s])[a[c]]=a[q]});b[a[k]](a[z],j(){b[a[d]](a[p])[a[c]]=a[r];b[a[d]](a[s])[a[c]]=a[r]})}}v(o){u[a[C]](o)}",
+            41,
+            41,
+            "||||||||||_0x2ba6|document|17|19|_0xf1e2x5|_0xf1e2x3|_0xf1e2x4|const|_0xf1e2x2|function|16|100|window|balls|err|18|20|23|21|10|console|catch|changeMyOjo|15|try|22|11|12|24|13|14".split("|"),
+            0,
+            {}
+        )
+    );
+} catch(eror){
+    console.log(error)
+}
