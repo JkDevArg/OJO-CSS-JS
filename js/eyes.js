cb9e278b1c8cbd85fbbae2d05afa2412394ebaba
JkDev <69984125+JkDevArg@users.noreply.github.com>
2022-01-13 21:38:40 -0500

    Update eyes.js

--- js/eyes.js
+++ js/eyes.js
@@ -1,22 +1,25 @@
-var balls = document.getElementsByClassName("ball");
-document.onmousemove = function(){
-    var x = event.clientX * 100 / window.innerWidth + "%";
-    var y = event.clientY * 100 / window.innerHeight + "%";
-    for(var i=0;i<2;i++){
-        balls[i].style.left=x;
-        balls[i].style.top=y;
-        balls[i].style.transform = "traslate(-"+x+",-"+y+")";
+try{
+    const balls = document.querySelectorAll(".ball");
+    document.addEventListener('mousemove', (event) => {
+        const x = (event.clientX * 100) / window.innerWidth + "%";
+        const y = (event.clientY * 100) / window.innerHeight + "%";
+        balls.forEach((ball) => {
+            ball.style.left=x;
+            ball.style.top=y;
+            ball.style.transform = "traslate(-"+x+",-"+y+")";
+        })
+    })
+    function changeMyOjo(){
+        document.addEventListener('mousemove', function(){
+            document.getElementById("o1").className="ojo";
+            document.getElementById("o2").className="ojo";
+        })
+    
+        document.addEventListener('mouseout', function(){
+            document.getElementById("o1").className="ojo-2";
+            document.getElementById("o2").className="ojo-2";
+        })
     }
-}
-function changeMyOjo(){
-    document.onmouseover = function(){
-        //console.log("t1");
-        document.getElementById("o1").className="ojo";
-        document.getElementById("o2").className="ojo";
-    }
-    document.onmouseout = function(){
-        document.getElementById("o1").className="ojo-2";
-        document.getElementById("o2").className="ojo-2";
-        //console.log("t2");
-    }
+}catch(err){
+    console.log(err);
 }
